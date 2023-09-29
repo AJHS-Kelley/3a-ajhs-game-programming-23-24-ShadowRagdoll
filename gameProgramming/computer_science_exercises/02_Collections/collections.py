47c790a48ede3d85c34058daeff87bbe7c723300
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-09-29 13:06:41 -0400

    v0.1a
    Declared and delined 3 lists

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,10 @@
 # Collections Examples, Lily King, v0.0
+
+# LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES
+breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
+# each item on the list is known as an ELEMENT.
+# the position in the list for each is the INDEX
+# the element "BACON" is at index 0
+# python only: index -1 it is the last item on the list
+testScores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
