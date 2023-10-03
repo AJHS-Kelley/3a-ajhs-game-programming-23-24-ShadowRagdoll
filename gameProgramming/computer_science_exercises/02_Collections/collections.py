9d9a05303b015c108ece9f6f949960da39c13bd6
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-10-03 11:44:40 -0400

    v0.1d
    WYOC -- Access 3rd Element
    Comment out print statements.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -16,12 +16,17 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 
 # Accessing Specific List Elements -- REMEMBER FIRST ELEMENT IS INDEX 0
 #print(breakfastFoods[0])
-#print(testScores[0])S
+#print(testScores[0])
 #print(classGPA[0])
 
 # Accessing Last Element in List
 #print(breakfastFoods[-1])
 #print(testScores[-1])
 #print(classGPA[-1])
+
+# Pause - WYOC -- Access the 3rd Element in Each List
+#print(breakfastFoods[2])
+#print(testScores[2])
+#print(classGPA[2])
 
 
