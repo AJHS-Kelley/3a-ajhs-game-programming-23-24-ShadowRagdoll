5912cb610e72312eecbaf88e769cf9ef03fee79e
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-10-03 11:30:14 -0400

    v0.1b
    Printing List Contents

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -8,3 +8,9 @@ breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
 # python only: index -1 it is the last item on the list
 testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+# Printing Contents of an List
+print(breakfastFoods)
+print(testScores)
+print(classGPA)
+
