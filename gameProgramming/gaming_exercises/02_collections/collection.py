df09500e7ef93fc7509612ce8b9c5caf560ac5a8
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-10-05 11:39:58 -0400

    10/5

--- gameProgramming/gaming_exercises/02_collections/collection.py
+++ gameProgramming/gaming_exercises/02_collections/collection.py
@@ -1,15 +1,36 @@
 # ADDING ITEMS
-playerInventory = []
-while len(playerInventory) < 10:
-    item = input("What do you want to add?\n")
-    playerInventory.append(item) # .append() adds to END of the list.
-playerInventory.sort()
+#playerInventory = []
+#while len(playerInventory) < 10:
+    #item = input("What do you want to add?\n")
+    #playerInventory.append(item) # .append() adds to END of the list.
+#playerInventory.sort()
 # .whatever() is known as a METHOD. It means "do this to that".
-print(playerInventory)
+#print(playerInventory)
 
 # REMOVE ITEMS
-while len(playerInventory) > 5:
-    item = input("What do you want to remove?\n")
-    playerInventory.remove(item)
-playerInventory.sort()
-print(playerInventory)
+#while len(playerInventory) > 5:
+    #item = input("What do you want to remove?\n")
+    #playerInventory.remove(item)
+#playerInventory.sort()
+#print(playerInventory)
+
+# FIXED INVENTORY SYSTEM
+weaponList = [
+    False, # sword
+    False, # pistol
+    False, # missile launcher
+    False, # laser blaster
+    False # ice beam
+]
+# each item/value in a list is an ELEMENT
+# the location of each element in the list is the INDEX
+# first element is index[0]
+# stortcut to last element is index[-1]
+
+
+
+
+# Item Exists in Inventory
+
+
+# Random Enemy Generator
