d4c6a2955b5e408eed0aefd82aa598cf947efb60
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-11-01 12:58:30 -0400

    11/1/23 class

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,4 +1,80 @@
+import random
 wordList =  'eggs bread toast beef cake blueberry candy butter apple noodles jam carrot potato beans steak dog cat fish bird rat bug bear mouse deer bunny ape racoon possum hippo cow'.split()
 # .split() will split a string into separate elements, by default based on SPACE
 
+# VARIABLE_NAMES that are ALL CAPS are CONSTANTS and not meant to change.
+HANGMAN_BOARD = ['''
+    +----+
+         |
+         |
+         |
+         |
+       =====''','''   
+    +----+
+    O    |
+         |
+         |
+         |
+       =====''','''
+    +----+
+    O    |
+    |    |
+         |
+         |
+       =====''','''
+    +----+
+    O    |
+   /|    |
+         |
+         |
+       =====''','''
+    +----+
+    O    |
+   /|\   |
+         |
+         |
+       =====''','''
+    +----+
+    O    |
+   /|\   |
+   /     |
+         |
+       =====''','''
+    +----+
+    O    |
+   /|\   |
+   / \   |
+         |
+       =====''']
 
+def getRandomWord(wordList):   
+    wordIndex = random.randint(0 , len(wordList) - 1)
+    # len(listName) - 1 IS THE MOST COMMON WAY TO PREVENT INDEX OUT OF RANGE ERRORS
+    #print(wordIndex)
+    return wordList[wordIndex]
+
+# i = 0
+# while i < 50:
+#     getRandomWord(wordList)
+#     i += 1
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
--- other.py
+++ other.py
@@ -2,6 +2,7 @@ firstChoice = ""
 secChoice = ""
 thirdChoice = ""
 fouthChoice = ""
+fifthChoice = ""
 youReady = 0
 print("Welcome to a normal day of life.\n")
 print("You wake up in your room.\n")
@@ -21,8 +22,25 @@ if input == "look around":
             if input == "make it your new table":
                 print("You take it inside and make it your new table, but when you go back outside you see the neighbor giving you the scariness stare you have ever seen.\n")
                 print("A death stare, it looks like he might actually kill you.\n")
+                fifthChoice = input("should you talk to them or run?\n")
+                if input == "run":
+                    print("You run as fast as you can away from him.\n")
+                    print("You look back as you are running to check to see if he is behind you.\n")
+                    print("When you do this though you bump into another neighbor.\n")
+                    print("She gives you the exact same kind of stare as the other neighbor.\n")
+                    print("A death stare.\n")
+                    print("Your Body Was Never Found Ending\n")
+                else:
+                    if input
             else:
-                if input == "make it "
+                if input == "make it your home":
+                    print("You decide that you don't want to work, ever again.\n")
+                    print("All you had to do was go to work so you can get money for rent, but you gave up and decided to live in a box.\n")
+                    print("At least it's rent free.\n")
+                    print("Box Sweet Box Ending\n")
+                else:
+                    print("Please type make it your new table or make it your home for your selection.\n")
+                    fouthChoice = input("Make it your new table or make it your home?\n")
         else:
             print("You now stand by your car.\n")
     else:
