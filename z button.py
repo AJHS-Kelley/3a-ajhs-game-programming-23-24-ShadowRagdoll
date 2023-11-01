831739d2140b6eda6cdad5433e12c458b6a11818
ShadowRagdoll <s11639564@students.duvalschools.org>
2023-11-01 15:30:19 -0400

    more fun

--- z button.py
+++ z button.py
@@ -10,7 +10,24 @@ window = Tk()
 button = Button(window,
                 text="click me",
                 command=click,
-                font=("Comic Sans", 30))
+                font=("Comic Sans", 30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE
+                )
+
+button2 = Button(window,
+                text="click me2",
+                command=click,
+                font=("Comic Sans", 30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE
+                )
 
 button.pack()
 
