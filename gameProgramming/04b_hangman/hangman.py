1012d4d0ae3e1e567aeb82408b9f788c0f59eaad
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-11-07 12:05:12 -0500

    11 7 lunch

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -125,7 +125,17 @@ while True: # 99% of the time the loop is done this way.
     if foundAllLetters:
       print('Congratulations! You have guessed correctly.\n')
       gameIsDone = True
+  else: # MISSED LETTER GUESS
+    missedLetters = missedLetters + guess
+
+    if len(missedLetters) == len(HANGMAN_BOARD) - 1:
+      displayBoard(missedLetters, correctLetters, secretWord)
+      print('You have lost due to using all guesses.\n')
+      print('The secret word was ' + secretWord)
+      gameIsDone = True
        
+    
+     
     
 
 
--- z my own stuff/z functionty.py
+++ z my own stuff/z functionty.py
@@ -1,2 +1,7 @@
 # function = A block of reusable code
 #            place () after the function name to invoke it
+
+print("Happy birthday to you!")
+print("You are old!")
+print("Happy birth to you!")
+print()
