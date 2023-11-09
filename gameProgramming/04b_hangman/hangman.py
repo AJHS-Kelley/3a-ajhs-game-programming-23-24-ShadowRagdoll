7e264cfe579659a78e163af8ae11f3f56223d883
Ryan Kelley <kelleyr@duvalschools.org>
2023-11-09 11:49:42 -0500

    CRT -- Hangman
    Your code was not correctly indented due to an error with Visual Studio Code.

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -81,21 +81,21 @@ def displayBoard(missedLetters, correctLetters, secretWord):
 def getGuess(alreadyGuessed):
   # Only allow: single character, A-Z only, not guessed already.
   while True: # defult 'infinite' loop
-    print('Guess a letter and press enter.')
-    guess = input()
-    guess = guess.lower() 
-    if len(guess) != 1:
-      print('Please enter a single letter.\n')
-    elif guess not in 'abcdefghijklmnopqrstuvwxyz':
-      print('Please enter an English letter only.')
-    elif guess in alreadyGuessed:
-      print('Already guessed. Guess a different letter!')
-  else:
-     return guess
+      print('Guess a letter and press enter.')
+      guess = input()
+      guess = guess.lower() 
+      if len(guess) != 1:
+          print('Please enter a single letter.\n')
+      elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+          print('Please enter an English letter only.')
+      elif guess in alreadyGuessed:
+          print('Already guessed. Guess a different letter!')
+      else:
+          return guess
 
 def playAgain():
-   print('Do you want to play again? Yes or No, then press enter.')
-   return input().lower().startswith('y') # return True/False based on input
+    print('Do you want to play again? Yes or No, then press enter.')
+    return input().lower().startswith('y') # return True/False based on input
 
 #   START THE ACTUAL GAME
 print('Let\'s Play Hangman!') # \ ESCAPES special characters.
@@ -109,39 +109,39 @@ gameIsDone = False
 while True: # 99% of the time the loop is done this way.
   # TWO WAYS TO EXIT while True: return OR break
   # DISPLAY BOARD
-  displayBoard(missedLetters, correctLetters, secretWord)
+    displayBoard(missedLetters, correctLetters, secretWord)
 
-  guess = getGuess(missedLetters + correctLetters)
+    guess = getGuess(missedLetters + correctLetters)
    
-  if guess in secretWord: # Is the guess in the secretWord?
-    correctLetters = correctLetters + guess
+    if guess in secretWord: # Is the guess in the secretWord?
+        correctLetters = correctLetters + guess
 
     # CHECK FOR VICTORY
-    foundAllLetters = True
-    for i in range(len(secretWord)):
-      if secretWord[i] not in correctLetters:
-        foundAllLetters = False
-        break
-    if foundAllLetters:
-      print('Congratulations! You have guessed correctly.\n')
-      gameIsDone = True
-  else: # MISSED LETTER GUESS
-    missedLetters = missedLetters + guess
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+          if secretWord[i] not in correctLetters:
+            foundAllLetters = False
+            break
+        if foundAllLetters:
+            print('Congratulations! You have guessed correctly.\n')
+            gameIsDone = True
+    else: # MISSED LETTER GUESS
+        missedLetters = missedLetters + guess
 
-    if len(missedLetters) == len(HANGMAN_BOARD) - 1:
-      displayBoard(missedLetters, correctLetters, secretWord)
-      print('You have lost due to using all guesses.\n')
-      print('The secret word was ' + secretWord)
-      gameIsDone = True
+        if len(missedLetters) == len(HANGMAN_BOARD) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord)
+            print('You have lost due to using all guesses.\n')
+            print('The secret word was ' + secretWord)
+            gameIsDone = True
       
-  if gameIsDone:
-    if playAgain(): 
-      secretWord = getRandomWord(wordList)
-      missedLetters = ''
-      correctLetters = ''
-      gameIsDone = False
-    else:
-      break
+    if gameIsDone:
+        if playAgain(): 
+            secretWord = getRandomWord(wordList)
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+        else:
+          break
 
      
     
