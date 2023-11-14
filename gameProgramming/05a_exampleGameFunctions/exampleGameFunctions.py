b87adefc5185f51a5182bcba7bda158687bd8166
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-11-14 11:58:31 -0500

    Update exampleGameFunctions.py

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,11 +1,36 @@
 # Example Game Functions, Lily King, v0.0
 import random
 
-def functionOne():
-    pass
+# VARIABLE DELCARATIONS
+numberSkill = 0 # Scale of 0 - 100, 100 being the best
+listTypes = ["idk",
+            "wah",
+            "idem"]
+otherStuff = None
+what = None
+
+def functionOne(numberSkill, listTypes):
+    if numberSkill > 90 and listTypes == "idk" and otherStuff == "bit" and what == "?":
+        sfk = random.randint(0, 100)
+        if sfk > 10:
+            print("Hit")
+        else:
+            print("Miss")
+    elif numberSkill > 90 and listTypes == "idk" and otherStuff == "bit" and what == "ahh":
+        sfk = random.randint(0, 100)
+        if sfk > 50:
+            print("Hit")
+        else:
+            print("Miss")
+
 def functionTwo():
     pass
+
 def functionThree():
     pass
+
 def functionFour():
     pass
+
+
+
