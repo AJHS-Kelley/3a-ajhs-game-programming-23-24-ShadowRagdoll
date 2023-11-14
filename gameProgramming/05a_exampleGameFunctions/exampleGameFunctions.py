3c80a2fb097c9a18664fb3e81208376ad3a3f0d7
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-11-14 11:42:50 -0500

    Update exampleGameFunctions.py

--- gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,11 @@
 # Example Game Functions, Lily King, v0.0
+import random
+
+def functionOne():
+    pass
+def functionTwo():
+    pass
+def functionThree():
+    pass
+def functionFour():
+    pass
