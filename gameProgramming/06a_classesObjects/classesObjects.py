71d4ed94f1939635c63e986d04a7baddb5e0c50f
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-12-11 11:31:47 -0500

    Update classesObjects.py

--- gameProgramming/06a_classesObjects/classesObjects.py
+++ gameProgramming/06a_classesObjects/classesObjects.py
@@ -1,1 +1,12 @@
 # Classes and Objects, Lily King, v0.0
+
+
+class Person: # Class names should be PascalCase
+    def __init__(self, age, height, hairColor, name, weight, birthday):
+        # The self keyword refers to the specific object you are dealing with.
+        self.age = age
+        self.height = height
+        self.hairColor = hairColor
+        self.name = name
+        self.weight = weight
+        self.birthday = birthday
