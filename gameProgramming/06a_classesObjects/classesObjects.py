cdaa5c39f8c3b809f1fefa94064de93d0b361b19
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-12-11 11:47:36 -0500

    Update classesObjects.py

--- gameProgramming/06a_classesObjects/classesObjects.py
+++ gameProgramming/06a_classesObjects/classesObjects.py
@@ -11,10 +11,15 @@ class Person: # Class names should be PascalCase
         self.weight = weight
         self.birthday = birthday
 
+    # __str__ Method Format
+    def __str__(self):
+        return f"{self.name} is {self.age} years old. \nThey have {self.hairColor} hair and weigh {self.weight} pounds. \nThey were born on {self.birthday} and stands {self.height} tall."
+
 # A class is a 'blueprint' to make an object.
 
 examplePerson = Person(6, "6'2\"", "black", "Pop", 70, "April 06")
 examplePerson2 = Person(90, "6'2\"", "brown", "Bop", 170, "July 06")
+print(examplePerson)
 print(examplePerson.name)
 print(examplePerson.age)
 print(examplePerson.height)
