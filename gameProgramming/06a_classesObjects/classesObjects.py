72564bd59a1a919e3e46c0c3ac52e726662896b9
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2023-12-11 11:40:06 -0500

    12/11 class

--- gameProgramming/06a_classesObjects/classesObjects.py
+++ gameProgramming/06a_classesObjects/classesObjects.py
@@ -10,3 +10,15 @@ class Person: # Class names should be PascalCase
         self.name = name
         self.weight = weight
         self.birthday = birthday
+
+# A class is a 'blueprint' to make an object.
+
+examplePerson = Person(6, "6'2\"", "black", "Pop", 70, "April 06")
+examplePerson2 = Person(90, "6'2\"", "brown", "Bop", 170, "July 06")
+print(examplePerson.name)
+print(examplePerson.age)
+print(examplePerson.height)
+
+print(examplePerson2.name)
+print(examplePerson2.hairColor)
+print(examplePerson2.age)
