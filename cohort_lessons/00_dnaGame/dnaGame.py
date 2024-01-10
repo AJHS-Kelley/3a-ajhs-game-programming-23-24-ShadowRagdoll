5637c3d52e4e4e63273d651b6834d8abe15fb6c9
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2024-01-10 12:05:23 -0500

    Create dnaGame.py

---
+++ cohort_lessons/00_dnaGame/dnaGame.py
@@ -0,0 +1,11 @@
+# DNA Replication, Lily King, v0.0a
+
+# Import Entire Module
+import time, datetime # BRING THE WHOLE TOOL BOX
+
+# Import Specific Method from a Module
+from random import choice # BEING JUST THE TOOL YOU NEED
+
+dnaBases = ["A", "T", "G", "C"]
+
+
