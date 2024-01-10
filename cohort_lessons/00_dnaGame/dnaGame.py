f77a15ee9fb866c670166e22b67c73af6663a5eb
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2024-01-10 13:03:26 -0500

    1/10 class

--- cohort_lessons/00_dnaGame/dnaGame.py
+++ cohort_lessons/00_dnaGame/dnaGame.py
@@ -6,6 +6,19 @@ import time, datetime # BRING THE WHOLE TOOL BOX
 # Import Specific Method from a Module
 from random import choice # BEING JUST THE TOOL YOU NEED
 
-dnaBases = ["A", "T", "G", "C"]
+dnaBases = ["A", "T", "G", "C"] # Adenine, Thymine, Guanine, Cyto
 
+def gameIntro() -> None:
+    pass
 
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a positive integer value for the number of bases to generate.\n"))
+    dnaSequence = ""
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    return dnaSequence
+
+#dna = genDNA()
+#print(dna)
