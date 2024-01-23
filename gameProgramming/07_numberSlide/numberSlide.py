853c208bffe7de2fca84c54f754f2578cc32b9ff
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2024-01-23 11:46:05 -0500

    Create numberSlide.py

---
+++ gameProgramming/07_numberSlide/numberSlide.py
@@ -0,0 +1,12 @@
+# Number Slide Game, Lily King, based on project by AI Sweigart, v0.0
+
+# GAME SETUP / STEPS 
+# Function to layout / setup the grid.
+# Create the tiles and add to board.
+# Check for Valid Movements.
+# Animate movement of tile.
+# Allow for user input to select tile (mouse click)
+# Identify location coordinates of mouse click, determine tile selected
+# Check for win -- All numbers in correct order.
+
+# DIVIDE AND CONQUER METHOD -- Break down a larger problem into smaller problems, then solve those.
