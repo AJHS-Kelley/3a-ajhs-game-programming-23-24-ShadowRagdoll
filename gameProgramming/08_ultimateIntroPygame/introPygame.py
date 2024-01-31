72b3bd05c2ca248672d8b50fbe6e79154f56eb58
ShadowRagdoll <lilyk5035@gmail.com>
2024-01-31 12:12:47 -0500

    5etrytgdf

--- gameProgramming/08_ultimateIntroPygame/introPygame.py
+++ gameProgramming/08_ultimateIntroPygame/introPygame.py
@@ -11,6 +11,9 @@ sky_surface = pygame.image.load('img/ultPy/Sky.png')
 ground_surface = pygame.image.load('img/ultPy/ground.png')
 text_surface = test_font.render('My game', False, 'Green')
 
+snail_surface = pygame.image.load('img/ultPy/Hand.png')
+snail_x_pos = 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,6 +23,7 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
+    screen.blit(hand_surface,(600,250))
 
     pygame.display.update()
     clock.tick(60)
