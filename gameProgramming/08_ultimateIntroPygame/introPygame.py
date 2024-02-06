f119161de852650e465f63508ecd883371a7db33
ShadowRagdoll <lilyk5035@gmail.com>
2024-02-06 11:40:14 -0500

    2 6 24 class

--- gameProgramming/08_ultimateIntroPygame/introPygame.py
+++ gameProgramming/08_ultimateIntroPygame/introPygame.py
@@ -8,8 +8,10 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('img/ultPy/Sky.png').convert()
-ground_surface = pygame.image.load('img/ultPy/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Green')
+ground_surface = pygame.image.load('img/ultPy/ground.png').convert
+
+score_surf = test_font.render('My game', False, 'Green')
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('img/ultPy/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600,300))
@@ -22,18 +24,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        #if event.type == pygame.MOUSEMOTION:
+            #if player_rect.collidepoint(event.pos): print('collision')
    
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+    screen.blit(score_surf,score_rect)
     
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
     screen.blit(snail_surf,snail_rect)
     screen.blit(player_surf,player_rect)
 
-    if player_rect.colliderect(snail_rect):
-        print('collision')
+    #if player_rect.colliderect(snail_rect):
+       # print('collision')
+    
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint(mouse_pos):
+       # print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
