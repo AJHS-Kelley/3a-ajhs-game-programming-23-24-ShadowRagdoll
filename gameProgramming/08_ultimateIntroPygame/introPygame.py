5e4448a9da880409ecfc8830e006ed1becd0da95
ShadowRagdoll <s11639564@students.duvalschools.org>
2024-02-06 18:42:14 -0500

    Update introPygame.py

--- gameProgramming/08_ultimateIntroPygame/introPygame.py
+++ gameProgramming/08_ultimateIntroPygame/introPygame.py
@@ -2,9 +2,11 @@ import pygame
 from sys import exit
 
 def display_score():
-    current_time = pygame.time.get_ticks()
-    score_surf = test_font
-    print(current_time)
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf,score_rect)
+    
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -12,6 +14,7 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load('img/ultPy/Sky.png').convert()
 ground_surface = pygame.image.load('img/ultPy/ground.png').convert
@@ -25,6 +28,7 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 player_surf = pygame.image.load('img/ultPy/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
+player_stand = 
 
 while True:
     for event in pygame.event.get():
@@ -44,6 +48,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         screen.blit(sky_surface,(0,0))
@@ -67,7 +72,7 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
         else:
-            screen.fill('Yellow')
+            screen.fill((94,129,162))
 
     pygame.display.update()
     clock.tick(60)
