dde7a63468c223f67dc2a43b3bb508c4547738f9
ShadowRagdoll <lilyk5035@gmail.com>
2024-04-04 13:24:51 -0400

    4 4 project

--- 9_finalProject/finalProject.py
+++ 9_finalProject/finalProject.py
@@ -6,10 +6,16 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption("Lucky's unlucky tail")
+clock = pygame.time.Clock()
+
+# probery have the cards be something seperade and be maybe be random
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
     # draw all our elements
     # update everything
     pygame.display.update()
+    clock.tick(60)
